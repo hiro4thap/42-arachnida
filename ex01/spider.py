7151fb0293dfac547ace341a89cc0725dd5c2f31
Hiroaki Ono <hiono@c2r4s2.42tokyo.jp>
2026-01-17 19:11:16 +0900

    added spider.py

---
+++ ex01/spider.py
@@ -0,0 +1,27 @@
+import requests
+import urllib.request
+
+url = 'https://www.python.org/static/img/python-logo.png'
+dst_path = './data/test.png'
+
+urllib.request.urlretrieve(url, dst_path)
+
+# handle options
+# search link
+#  recursively jump
+#  search images
+#  set image name
+# request.get
+# save images
+
+def find_images():
+    pass
+
+def set_options():
+    pass
+
+def find_links():
+    pass
+
+def save_image():
+    pass
